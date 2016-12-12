3134d730589c97b3a7f3b98294c8ed1c5cfc14d5
Mironychev P <mironychevpavel@gmail.com>
2016-12-13 01:50:04 +0300

    String. Exc.3 (add cycle for). 12.12.16

--- String/Exercise3.js
+++ String/Exercise3.js
@@ -2,14 +2,26 @@
 	//Имеем 2 переменные, строку и букву. 
 	//Вернуть количество вхождений буквы в строке.
 
-var a = new String("What a wonderfull world"), i = 0, n = 0;
-console.log(n);
+var a = "What a wonderfull world", i = 0, nWhile = 0, nFor = 0;
+
+console.log(a,'\nLookin for "w"\n');
+console.log("Cycle WHILE:");
 while (i <= a.length) {
 	if (a.charAt(i) === "w" || a.charAt(i) === "W") {
-		n++;
-		console.log("n = ", n);
+		nWhile++;
+		console.log("n (while) = ", nWhile);
 	}
-		console.log("i = ", i);
 	i++;
 }
-console.log("end count:",n)
+i = 0;
+console.log("\nCycle FOR:");
+a = a.toLowerCase();
+for (i = 0; i <= a.length; i++) {
+	if (a.charAt(i) === "w") {
+		nFor++;
+		console.log("n (for) = ", nFor);
+	}
+}
+console.log("end count (while):", nWhile, "\nend count (for):", nFor);
+
+
