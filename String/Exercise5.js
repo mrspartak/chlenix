ba3113ac0f1ff09f09585c0b147e9e3b0df01ab0
Assorium <mrspartak@users.noreply.github.com>
2016-12-14 21:53:04 +0300

    Update Exercise5.js

--- String/Exercise5.js
+++ String/Exercise5.js
@@ -7,3 +7,14 @@ for (var i = 1; i <= (str.length - 1); i = i + 2) {
 	str = str.join('');
 }
 console.log(str);
+
+/*
+1. Не усложняй
+for (var i = 1; i < str.length; i += 2)
+
+2. Каждый раз в цикле ты делаешь 2 накладные операции, когда мог их сделать за циклом
+var str = 'ciu33cb23093jch3';
+str = str.split('');
+for (var i = 1; i < str.length; i += 2) str[i] = 0;
+str = str.join('');
+*/
