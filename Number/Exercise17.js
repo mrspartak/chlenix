46d85a3d7ad70fdc6b0b635dff4bb8febc7d0016
Mironychev P <mironychevpavel@gmail.com>
2016-12-14 14:47:24 +0300

    Exc.17 Update

--- Number/Exercise17.js
+++ Number/Exercise17.js
@@ -1,4 +1,4 @@
 // Числа
 	// Имеем 2 числа, посчитать количество целых чисел между ними
-var a = 1.4, b = 5;
-console.log(Math.abs(Math.ceil(b)-Math.floor(a)-1));
+var a = 2, b = 3;
+console.log(Math.abs(Math.ceil(b)-Math.floor(a))-1);
