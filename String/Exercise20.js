c7ce63e5800c7809d4afe2ef6d2463027280ac36
fortuns <mironychevpavel@gmail.com>
2016-12-17 21:50:33 +0300

    Exc.20, uodate

--- String/Exercise20.js
+++ String/Exercise20.js
@@ -1,10 +1,15 @@
 // Строки
 // Превратить строку, где слова разделены нижним подчеркиванием в Camel Case
-var str = 'iam_dog_tail', i = str.indexOf('_');
-while (i <= str.length) {
+var str = 'iam_dog_tail_', i = str.indexOf('_');
+while (i < str.length) {
 	if (~str.indexOf('_')) {
-		str = str.slice(0, i) + str[i+1].toUpperCase() + str.slice(i+2);
-		i = str.indexOf('_');
+		if (str.indexOf('_') === str.length - 1) {
+			str = str.slice(0, i); 
+		}
+		else {
+			str = str.slice(0, i) + str[i+1].toUpperCase() + str.slice(i+2);
+			i = str.indexOf('_');
+		}
 	}
 	else break;
 }
