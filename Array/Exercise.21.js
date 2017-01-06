92b753a257180d3747702e59b8715942af83c4b6
fortuns <mironychevpavel@gmail.com>
2017-01-06 21:30:08 +0300

    Exc.20. v.1

---
+++ Array/Exercise.21.js
@@ -0,0 +1,17 @@
+// Массивы
+// Есть массив чисел. 
+// Определить среднее значение и среднеквадратичное отклонение. #21
+
+var firstArray = [-1, 0, 1, 1], sum = 0, average = 0, deviationSum = 0, standartDeviation = 0;
+
+for ( var i = 0; i < firstArray.length; i++) {
+	sum += firstArray[i];
+}
+average = sum / firstArray.length;
+for ( var i = 0; i < firstArray.length; i++) {
+	deviationSum += Math.pow(firstArray[i] - average, 2);
+	console.log(deviationSum);
+}
+standartDeviation = Math.sqrt(deviationSum / firstArray.length);
+console.log('Average: ', average);
+console.log('Standart deviation: ', standartDeviation);
