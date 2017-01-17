2dcfd72de6be6f2e2d773a48fd5653680f7b6fcd
Mironychev P <mironychevpavel@gmail.com>
2017-01-17 18:57:25 +0300

    Exc. 23

---
+++ Function/Exercise.23.js
@@ -0,0 +1,12 @@
+// Функции
+// Функция, которая принимает количество секунд и через столько секунд выдаст в консоль сообщение. #23
+// Пример:
+// sleep(5);
+// через 5 секунд, что-то должно отобразиться в консоли
+
+sleep( 5 );
+
+function sleep( time ) {
+	var delay = time * 1000;
+	setTimeout( function( ) { console.log('Any string with helpfull information') }, delay );
+}
