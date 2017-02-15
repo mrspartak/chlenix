5fd9f12f8192afb01ceea2dead98e84b9fc718ad
Mironychev P <mironychevpavel@gmail.com>
2017-02-15 12:43:28 +0300

    exc.22 v.3

--- Object/Exercise.22.js
+++ Object/Exercise.22.js
@@ -7,9 +7,10 @@
 // StringHelper.revert('not_camel');
 // lemac_ton
 
-var StringHelper = {}, stringExample = '_sunny_and_happy_day_';
+var StringHelper = new String();
+var stringExample = '_sunny__and_happy_day_';
 
-Object.prototype.toCamel = function(string) {
+String.prototype.toCamel = function(string) {
 	var i = string.indexOf('_'), _foundPos;
 	
 	while (true) {
@@ -29,11 +30,39 @@ Object.prototype.toCamel = function(string) {
 	return string;
 }
 
-Object.prototype.revert = function(string) {
+String.prototype.revert = function(string) {
 	return string = string.split('').reverse().join('');
 }
 
-
+console.log('Wrong method');
 console.log('String to camel case: ' + StringHelper.toCamel(stringExample) );
 console.log('Reverse string: ' + StringHelper.revert(stringExample) );
 
+
+var StringHelper2 = {
+	toCamel: function(string) {
+		var i = string.indexOf('_'), _foundPos;
+		
+		while (true) {
+			_foundPos = string.indexOf('_', i);
+
+			if (_foundPos === -1) break;
+
+			if (i === 0) {
+				string = string.substring(1);
+			} else if (i === (string.length - 1) ) {
+				string = string.substring(0, string.length - 1);
+			} else {
+				string = string.slice(0, i) + string.charAt(i + 1).toUpperCase() + string.slice(i + 2);
+			}
+			i = string.indexOf('_');
+		}
+		return string;
+	},
+	revert: function(string) {
+		return string = string.split('').reverse().join('');
+	}
+}
+console.log('\nCorrect method');
+console.log('String to camel case: ' + StringHelper2.toCamel(stringExample) );
+console.log('Reverse string: ' + StringHelper2.revert(stringExample) );
