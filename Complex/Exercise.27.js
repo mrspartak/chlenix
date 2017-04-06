e0b3273f24bc8865129c41cccd4e9a56f787a9cb
fortuns <mironychevpavel@gmail.com>
2017-04-06 11:58:41 +0300

    fix

--- Complex/Exercise.27.js
+++ Complex/Exercise.27.js
@@ -6,19 +6,21 @@
 // Ввод: str = 'Hi {name}'; obj = {name: 'Pavel'};
 // Вывод: Hi Pavel
 
-var str = 'Hello my dear friend, {name}!', obj = {name: 'Spartak'};
+var str = 'Hello my dear friend, {firstName} {secondName}. You have {numberOfAnimals} {animal}!', obj = {firstName: 'Kagarmanyan', secondName: 'Spartak', animal: 'cats', numberOfAnimals: 'two'};
 
 console.log( templateString(str, obj) );
 
 function templateString(string, object) {
 	var _a, _b;
 
-	_a = string.match( /{\w+}/i );
-	_b = _a[0].slice(1,-1);
+	_a = string.match( /{\w+}/ig );
 
-	if ( object.hasOwnProperty(_b) ) {
-		return string.replace(_a, object[_b]);
-	} else {
-		return string;
+	for (var i = 0; i < _a.length; i++) {
+		_b = _a[i].slice(1,-1);
+
+		if ( object.hasOwnProperty(_b) ) {
+			string = string.replace(_a[i], object[_b]);
+		}
 	}
+	return string;
 }
